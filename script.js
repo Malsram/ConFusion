94eb9e013a472b390b3299c9578aab5ec2296961
Ram <ramprasad.ramamurthy@accenture.com>
2018-05-03 11:15:10 +0530

    npm scripts

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $("#loginscript").click(function() {
+        $("#loginModal").modal();
+    });
+
+    $("#reservescript").click(function() {
+        $("#ReserveModal").modal();
+    });
+
+    $("#mycarousel").carousel({interval:2000});
+
+    $("#carouselButton").click(function(){
+      if ($("#carouselButton").children("span").hasClass('fa-pause')){
+          $("#mycarousel").carousel('pause');
+          $("#carouselButton").children("span").removeClass('fa-pause');
+          $("#carouselButton").children("span").addClass('fa-play');
+      }
+      else if ($("#carouselButton").children("span").hasClass('fa-play')){
+          $("#mycarousel").carousel('cycle');
+          $("#carouselButton").children("span").removeClass('fa-play');
+          $("#carouselButton").children("span").addClass('fa-pause');
+      }
+    });
+});
